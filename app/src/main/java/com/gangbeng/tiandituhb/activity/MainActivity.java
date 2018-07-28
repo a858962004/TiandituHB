14c1cae2acd3fe07969c8d56a3751930e3853456
Administrator <858962004@qq.com>
2018-07-28 23:47:11 +0800

    地图首页

--- app/src/main/java/com/gangbeng/tiandituhb/activity/MainActivity.java
+++ app/src/main/java/com/gangbeng/tiandituhb/activity/MainActivity.java
@@ -1,37 +1,67 @@
 package com.gangbeng.tiandituhb.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.gangbeng.tiandituhb.R;
-import com.gangbeng.tiandituhb.widget.CustomViewPager;
+import com.gangbeng.tiandituhb.base.BaseActivity;
+import com.tianditu.android.maps.GeoPoint;
+import com.tianditu.android.maps.MapController;
+import com.tianditu.android.maps.MapView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+    @BindView(R.id.bmapsView)
+    MapView bmapsView;
+    @BindView(R.id.bt_around)
+    Button btAround;
+    @BindView(R.id.bt_route)
+    Button btRoute;
+    @BindView(R.id.bt_set)
+    Button btSet;
+    @BindView(R.id.ll_searchview)
+    LinearLayout llSearchview;
 
-    @BindView(R.id.tablayout_main)
-    TabLayout tablayoutMain;
-    @BindView(R.id.customviewpager_main)
-    CustomViewPager customviewpagerMain;
+    @Override
+    protected void initView() {
+        setContentLayout(R.layout.activity_main);
+        setToolbarVisibility(false);
+        //设置启用内置的缩放控件
+        bmapsView.setBuiltInZoomControls(true);
+        //得到mMapView的控制权,可以用它控制和驱动平移和缩放
+        MapController mMapController = bmapsView.getController();
+        //用给定的经纬度构造一个GeoPoint，单位是微度 (度 * 1E6)
+        GeoPoint point = new GeoPoint((int) (39.915 * 1E6), (int) (116.404 * 1E6));
+        //设置地图中心点
+        mMapController.setCenter(point);
+        //设置地图zoom级别
+        mMapController.setZoom(12);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // TODO: add setContentView(...) invocation
         ButterKnife.bind(this);
-//        MapView mMapView = (MapView) findViewById(R.id.bmapsView);
-////设置启用内置的缩放控件
-//        mMapView.setBuiltInZoomControls(true);
-////得到mMapView的控制权,可以用它控制和驱动平移和缩放
-//        MapController mMapController = mMapView.getController();
-////用给定的经纬度构造一个GeoPoint，单位是微度 (度 * 1E6)
-//        GeoPoint point = new GeoPoint((int) (39.915 * 1E6), (int) (116.404 * 1E6));
-////设置地图中心点
-//        mMapController.setCenter(point);
-////设置地图zoom级别
-//        mMapController.setZoom(12);
+    }
+
+    @OnClick({R.id.bt_around, R.id.bt_route, R.id.bt_set, R.id.ll_searchview})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.bt_around:
+                break;
+            case R.id.bt_route:
+                break;
+            case R.id.bt_set:
+                break;
+            case R.id.ll_searchview:
+                break;
+        }
     }
 }
