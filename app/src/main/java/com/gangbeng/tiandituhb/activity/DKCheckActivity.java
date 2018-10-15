2a3d01eb2edde9dad0eb607748e31f9a64ff39d7
Administrator <858962004@qq.com>
2018-10-15 21:19:51 +0800

    地块核查界面

---
+++ app/src/main/java/com/gangbeng/tiandituhb/activity/DKCheckActivity.java
@@ -0,0 +1,16 @@
+package com.gangbeng.tiandituhb.activity;
+
+import com.gangbeng.tiandituhb.R;
+import com.gangbeng.tiandituhb.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2018/10/15.
+ */
+
+public class DKCheckActivity extends BaseActivity {
+    @Override
+    protected void initView() {
+        setContentLayout(R.layout.activity_dkcheck);
+
+    }
+}
